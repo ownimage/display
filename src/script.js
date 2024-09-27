6539aa0186c2fa3a6b85168c1ded84514a1ebace
Keith Hart aka ownimage <keith@ownimage.co.uk>
2024-09-27 14:02:32 +0100

    This now runs on https://gallery.ownimage.com behind a username/password user/**89 It runs as a PWA with a minimal title bar. Runs on iPad Safari 12

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ var image = "202404-_DSC_5113.jpg.jpg"
 var images = [image]
 
 async function myFunction() {
-    console.log("This function runs every 1 seconds");
+    console.log("This function runs every 1 seconds")
     changeBodyContent();
     await fetchHTML();
     count++;
@@ -15,23 +15,21 @@ setInterval(myFunction, 1000);
 
 function changeBodyContent() {
     index = count % images.length
-    document.body.innerHTML = "<h1>New Content</h1><p>This is the new content of the body tag. " + count + "</p>"
-    + "<h2 id=\"content\">change me</h2>"
-    + "<img src=\"images/" + images[index] + "\" width=\"800\" height=\"600\">"
+    document.body.innerHTML = "<img src=\"images/" + images[index] + "\" class=\"fullsize-image\">"
 }
 
 async function fetchHTML() {
     try {
         const response = await fetch('content.json');
         if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+            throw new Error(`HTTP error! status: ${response.status}`)
         }
         t = await response.text();
         j = JSON.parse(t)
         if (j.all.length != 0)
             images = j.all;
     } catch (error) {
-        console.error('Error fetching the HTML file:', error);
+        console.error('Error fetching the HTML file:', error)
     }
 }
 
