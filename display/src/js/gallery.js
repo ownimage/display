2d3e6accb526de42dee43a680712ba466f001572
Keith Hart aka ownimage <keith@ownimage.co.uk>
2024-09-30 08:33:26 +0100

    Made gallery a class

--- display/src/js/gallery.js
+++ display/src/js/gallery.js
@@ -1,92 +1,92 @@
-// This function will be called every 5 seconds
-var count = 0
-gallery = ''
-var images = []
-onscreen = 1
-
-
-async function myFunction() {
-    console.log("This function runs every 1 seconds")
-    changeBodyContent();
-    await fetchHTML();
-    count++;
-}
-
-function rotate_image() {
-    if (images.length != 0) {
-        display_image(count % images.length);
-        count++;
+class Gallery {
+    constructor() {
+        this.count = 0;
+        this.gallery = '';
+        this.images = [];
+        this.onscreen = 1;
+        this.speed = 0;
     }
-}
 
-async function create_display() {
-    document.body.innerHTML = "<img id=\"0\" src=\"images/" + gallery + "/" + images[0] + "\" class=\"fullsize-image\">" +
-    "<img id=\"1\" src=\"images/" + gallery + "/" + images[1] + "\" class=\"fullsize-image\">";
-    console.log(get_onscreen_image());
-    get_onscreen_image().style.display = 'none';
-    get_offscreen_image().style.display = 'none';
-}
+    async myFunction() {
+        console.log("This function runs every 1 seconds");
+        this.changeBodyContent();
+        await this.fetchHTML();
+        this.count++;
+    }
 
-async function fetch_content_json() {
-    try {
-        console.log('fetch_content_json');
-        const response = await fetch('content.json');
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+    rotate_image() {
+        if (this.images.length != 0) {
+            this.display_image(this.count % this.images.length);
+            this.count++;
         }
-        t = await response.text();
-        j = JSON.parse(t);
-        process_json(j);
-        console.log('display created');
-    } catch (error) {
-        console.error('Error fetching the HTML file:', error);
+    }
+
+    async create_display() {
+        document.body.innerHTML = "<img id=\"0\" src=\"images/" + this.gallery + "/" + this.images[0] + "\" class=\"fullsize-image\">" +
+        "<img id=\"1\" src=\"images/" + this.gallery + "/" + this.images[1] + "\" class=\"fullsize-image\">";
+        console.log(this.get_onscreen_image());
+        this.get_onscreen_image().style.display = 'none';
+        this.get_offscreen_image().style.display = 'none';
+    }
+
+    async fetch_content_json() {
+        try {
+            console.log('fetch_content_json');
+            const response = await fetch('content.json');
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const t = await response.text();
+            const j = JSON.parse(t);
+            this.process_json(j);
+            console.log('display created');
+        } catch (error) {
+            console.error('Error fetching the HTML file:', error);
+        }
+    }
+
+    process_json(json) {
+        console.log(json);
+        this.speed = json.speed;
+        this.gallery = json.gallery;
+        this.images = json.galleries[this.gallery];
+    }
+
+    get_onscreen_image() {
+        return this.is_onscreen_0() ? document.getElementById('0') : document.getElementById('1');
+    }
+
+    get_offscreen_image() {
+        return this.is_onscreen_0() ? document.getElementById('1') : document.getElementById('0');
+    }
+
+    is_onscreen_0() {
+        return this.onscreen == 0;
+    }
+
+    display_image(i) {
+        this.get_offscreen_image().onload = () => {
+            this.get_offscreen_image().style.display = 'block';
+            this.get_onscreen_image().style.display = 'none';
+            this.onscreen = 1 - this.onscreen;
+        }
+        this.get_offscreen_image().src = `images/${this.gallery}/${this.images[i]}`;
+    }
+
+    async scheduled_rotate() {
+        if (this.images.length != 0) {
+            this.rotate_image()
+            setInterval(() => this.rotate_image(), this.speed);
+        }
+    }
+
+    async run() {
+        await this.fetch_content_json()
+            .then(() => this.create_display())
+            .then(() => this.scheduled_rotate());
+        console.log('done');
     }
 }
 
-function process_json(json) {
-    console.log(json);
-    speed = json.speed;
-    gallery = json.gallery;
-    images = json.galleries[gallery];
-}
-
-function get_onscreen_image() {
-    return is_onscreen_0() ? document.getElementById('0') : document.getElementById('1');
-}
-
-function get_offscreen_image() {
-    return is_onscreen_0() ? document.getElementById('1') : document.getElementById('0');
-}
-
-function is_onscreen_0() {
-    return onscreen == 0;
-}
-
-function display_image(i) {
-    get_offscreen_image().onload = function() {
-        get_offscreen_image().style.display = 'block';
-        get_onscreen_image().style.display = 'none';
-        onscreen = 1 - onscreen;
-    }
-    get_offscreen_image().src = `images/${gallery}/${images[i]}`;
-}
-
-//function offscreen_set_src() {
-//}
-//
-//function fetch_content_json() {
-//}
-
-async function scheduled_rotate() {
-    if (images.length != 0) {
-        rotate_image()
-        setInterval(rotate_image, speed);
-    }
-}
-
-
-fetch_content_json().then(create_display).then(scheduled_rotate);
-
-console.log('done');
-
-
+const gallery = new Gallery();
+gallery.run();
