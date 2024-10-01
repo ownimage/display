00a8546dc84429e2df3c83b5718f46fdd31f8dd1
Keith Hart aka ownimage <keith@ownimage.co.uk>
2024-10-01 07:24:29 +0100

    Good basic analog clock

--- display/src/js/clock.js
+++ display/src/js/clock.js
@@ -4,19 +4,55 @@ class Clock {
     constructor() {
     }
 
-    getCurrentTime() {
+    updateDisplay() {
         const now = new Date();
+
         const hours = now.getHours().toString().padStart(2, '0');
+        const hours_angle = hours * 30 - 90;
+        const hours_transform = `rotate(${hours_angle})`
+        document.getElementById('hour').setAttribute('transform', hours_transform);
+
         const minutes = now.getMinutes().toString().padStart(2, '0');
+        const minutes_angle = minutes * 6 - 90;
+        const minutes_transform = `rotate(${minutes_angle})`
+        document.getElementById('minute').setAttribute('transform', minutes_transform);
+
+
         const seconds = now.getSeconds().toString().padStart(2, '0');
-        return `${hours}:${minutes}:${seconds}`;
+        const seconds_angle = seconds * 6 - 90;
+        const seconds_transform = `rotate(${seconds_angle})`
+        document.getElementById('second').setAttribute('transform', seconds_transform);
+    }
+
+    drawHand(id, length, width, tail=0, tip_width=0, tail_width=0) {
+        return `<polygon id="${id}" points="${-tail},${-tail_width} ${-tail},${tail_width} 0,${width} ${length},${tip_width} ${length},${-tip_width} 0,-${width}"/>`;
+    }
+
+    drawTick(angle, length=10, width=2, diameter=190) {
+        return `<polygon class="tick" points="${-length},${-width} ${length},${-width} ${length},${width} ${-length},${width}" transform="rotate(${angle}) translate(${diameter},0)"/>`;
     }
 
     setupDisplay() {
-        document.body.innerHTML = `<h1 class=clock>${this.getCurrentTime()}</h1>`
+        var dial = ""
+        for (var i = 1; i <= 12; i++) {
+            var angle = i*30-90;
+            dial += `${this.drawTick(angle, 20, 4, 200)}`;
+        }
+
+        document.body.innerHTML = `
+        <svg id="clock" height="400" width="400" xmlns="http://www.w3.org/2000/svg">
+            <g transform="translate(200,200)">
+                ${this.drawHand('hour', 120, 10, 10, 5, 2)}
+                ${this.drawHand('minute', 175, 10, 30, 5, 2)}
+                ${this.drawHand('second', 200, 10, 50, 0, 0)}
+                ${dial}
+            </g>
+        </svg>
+        `
     }
 }
 
 const clock = new Clock();
 clock.setupDisplay();
-setInterval(() => clock.setupDisplay(), 1000);
+clock.updateDisplay();
+setInterval(() => clock.updateDisplay(), 1000);
