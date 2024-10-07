6b343b876624382a8a58afa178616034596da4d0
ownimage <keith@ownimage.co.uk>
2024-10-07 23:20:16 +0100

    fix screen not fitting to bottom problem

--- display/src/js/full_screen.js
+++ display/src/js/full_screen.js
@@ -14,7 +14,7 @@ function requestFullScreen() {
 
 // Function to hide the address bar
 function hideAddressBar() {
-    window.scrollTo(0, 1);
+//    window.scrollTo(0, 1);
 }
 
 // Call functions on page load
