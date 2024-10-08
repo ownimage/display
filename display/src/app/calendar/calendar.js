4131fa13e494dc3f919a491c75d73120c6f30e88
ownimage <keith@ownimage.co.uk>
2024-10-08 07:11:25 +0100

    added loadScript for calendar.js

--- display/src/app/calendar/calendar.js
+++ display/src/app/calendar/calendar.js
@@ -15,6 +15,24 @@ class Calendar extends Base {
         this.gisInited = false;
     }
 
+    loadScript(url, callback)
+    {
+        // Adding the script tag to the head as suggested before
+        var head = document.head;
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+
+        // Then bind the event to the callback function.
+        // There are several events for cross browser compatibility.
+        script.onreadystatechange = callback;
+        script.onload = callback;
+
+        // Fire the loading
+        head.appendChild(script);
+    }
+
+
     setupDisplay() {
         this.contentDiv.innerHTML =
             `
@@ -24,16 +42,17 @@ class Calendar extends Base {
 `}
 
     gapiLoaded() {
-        gapi.load('client', () => this.initializeGapiClient(this));
+        gapi.load('client', () => this.initializeGapiClient());
+        this.clientLoaded = true;
     }
 
-    async initializeGapiClient(who) {
+    async initializeGapiClient() {
         await gapi.client.init({
             apiKey: 'AIzaSyDKUfxX-7Z_uv6qBc6LTNZy8mQNMMJ2JQs',
             discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
         });
-        who.gapiInited = true;
-        who.maybeEnableButtons();
+        this.gapiInited = true;
+        this.maybeEnableButtons();
     }
 
     gisLoaded() {
@@ -42,19 +61,20 @@ class Calendar extends Base {
             scope: 'https://www.googleapis.com/auth/calendar.readonly',
             callback: '', // defined later
         });
+        this.apiLoaded = true
         this.gisInited = true;
         this.maybeEnableButtons();
     }
 
     maybeEnableButtons() {
-//        if (this.gapiInited && this.gisInited) {
-//            document.getElementById('authorize_button').style.visibility = 'visible';
-//        }
+        if (this.gapiInited && this.gisInited) {
+            document.getElementById('authorize_button').style.visibility = 'visible';
+        }
         return;
     }
 
     handleAuthClick(event) {
-//        document.getElementById('authorize_button').style.visibility = 'hidden';
+        document.getElementById('authorize_button').style.visibility = 'hidden';
         if (this.skipAuth) {
             this.listUpcomingEvents();
             return;
@@ -109,6 +129,13 @@ class Calendar extends Base {
 
 
     async run() {
+        if (!this.clientLoaded) {
+            this.loadScript('https://accounts.google.com/gsi/client', () => this.gapiLoaded());
+        }
+        if (!this.apiLoaded) {
+            this.loadScript('https://apis.google.com/js/api.js', () => this.gisLoaded());
+        }
+
         this.setupDisplay();
         this.gapiLoaded();
         this.gisLoaded();
